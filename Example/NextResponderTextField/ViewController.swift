e69a70299594af5f928ca0f8c3c76d4180d6210e
Mohamed Afifi <mohamede1945@gmail.com>
2015-06-28 18:06:05 +0200

    Example code.

--- Example/NextResponderTextField/ViewController.swift
+++ Example/NextResponderTextField/ViewController.swift
@@ -10,14 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func submitButtonTapped(sender: AnyObject) {
+        UIAlertView(title: "", message: "Submit button tapped", delegate: nil, cancelButtonTitle: "Ok").show()
     }
 
 }
